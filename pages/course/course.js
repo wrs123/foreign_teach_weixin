527c54a27a487d18be7b654f870f1242ec19821e
ofg <1309870573@qq.com>
2020-10-22 16:24:54 +0800

    edit:修改全部课程页面

--- pages/course/course.js
+++ pages/course/course.js
@@ -27,13 +27,48 @@ Page({
     screenWidth: app.globalData.screenWidth,
     contentWidth: app.globalData.screenWidth * 0.75,
     imageWidth: app.globalData.screenWidth * 0.75 * .35,
-    imageHeight: (app.globalData.screenWidth * 0.75 * .35/16)*11
+    imageHeight: (app.globalData.screenWidth * 0.75 * .35/16)*11,
+    isLoading: true,
+    scrollTop: 0,
+    modelZIndex: 1
   },
   onLoad(event){
     this.changeNavType()
+    let that = this
+    setTimeout(function(){
+      that.setData({
+        isLoading: false
+      })
+      setTimeout(function(){
+        that.setData({
+          modelZIndex: -1
+        })
+      },300)
+    },2000)
+    
+    
   },
   onTabChange: function(e){
     console.log(e);
+    let id = e.detail.id
+    let that = this
+
+    this.setData({
+      isLoading: true,
+      modelZIndex: 1
+    })
+    setTimeout(function(){
+      that.setData({
+        isLoading: false,
+        scrollTop: 0
+      })
+      setTimeout(function(){
+        that.setData({
+          modelZIndex: -1
+        })
+      },300)
+    },2000)
+    
   },
   changeNavType: function(){
     wx.setNavigationBarColor({
